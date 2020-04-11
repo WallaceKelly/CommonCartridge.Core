224ca0e6c2ce70866c68311d64ed3b74234be77c
Wallace Kelly <wallace@bluerockresearch.com>
2020-04-11 17:13:17 -0400

    Create initial unit tests to load IMSCC manifests

---
+++ src/CommonCartridge.Core.Lib/Manifest.fs
@@ -0,0 +1,5 @@
+﻿namespace CommonCartridge
+
+open FSharp.Data
+
+type Manifest = XmlProvider< "../../schema/ccv1p1_lommanifest_v1p0.xsd" >
---
+++ tests/CommonCartridge.Core.Lib.Tests/ManifestTests.fs
@@ -0,0 +1,27 @@
+﻿module CommonCartridge.ManifestTests
+
+open Xunit
+open System.IO
+open XmlDiffLib
+
+[<Fact>]
+let ``Loads empty manifest without throwing``() =
+    Manifest.Load("imsmanifest.xml")
+    |> ignore
+
+[<Fact>]
+let ``Saves unmodified empty manifest as identical file``() =
+    
+    let srcFilePath = "imsmanifest.xml"
+    let dstFilePath = Path.GetTempFileName()
+    try
+        let manifest = Manifest.Load(srcFilePath)
+        manifest.XElement.Save(dstFilePath)
+        let srcFile = File.ReadAllText(srcFilePath)
+        let dstFile = File.ReadAllText(srcFilePath)
+        let diff = new XmlDiff(srcFile, dstFile)
+        diff.CompareDocuments(XmlDiffOptions(TwoWayMatch = true)) |> ignore
+        Assert.Empty(diff.DiffNodeList)
+    finally
+        if File.Exists dstFilePath then
+            File.Delete dstFilePath
