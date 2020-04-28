25d8911545c219f3054e245b4e12841a053f5847
Wallace Kelly <wallace@bluerockresearch.com>
2020-04-27 21:24:23 -0400

    reads activity list

--- tests/CommonCartridge.Word.Lib.Tests/WordModulesTests.fs
+++ tests/CommonCartridge.Word.Lib.Tests/WordModulesTests.fs
@@ -11,7 +11,7 @@ let ``WordModules.append appends a module to a Word file`` () =
     let path = Path.GetTempFileName()
     try
         let wordFile = ImsccWordFile.create path
-        let modul = { ImsccModule.Title = "Module A" }
+        let modul = { ImsccModule.Title = "Module A"; Identifier = ""; Items = [||] }
         // act
         modul |> WordModule.append wordFile
         wordFile |> ImsccWordFile.saveFile
@@ -22,6 +22,28 @@ let ``WordModules.append appends a module to a Word file`` () =
             |> Seq.exists(fun p ->
                 p.Runs
                 |> Seq.exists(fun r -> r.Text.Contains("Module A")))
+        )
+    finally
+        File.Delete(path)
+
+[<Fact>]
+let ``WordModules.append appends a activities to a Word file`` () =
+    // arrange
+    let path = Path.GetTempFileName()
+    try
+        let wordFile = ImsccWordFile.create path
+        let activity = { ImsccModuleItem.Title = "Activity 1"; Identifier = ""; IdentifierRef = "" }
+        let modul = { ImsccModule.Title = "Module A"; Identifier = ""; Items = [| activity |] }
+        // act
+        modul |> WordModule.append wordFile
+        wordFile |> ImsccWordFile.saveFile
+        // assert
+        let wordFile2 = ImsccWordFile.openFile path
+        Assert.True(
+            wordFile2.Document.Paragraphs
+            |> Seq.exists(fun p ->
+                p.Runs
+                |> Seq.exists(fun r -> r.Text.Contains("Activity 1")))
         )
     finally
         File.Delete(path)
